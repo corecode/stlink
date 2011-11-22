4d30fc35eb659c728570c13977f13e467d8e5654
Artem Borisovskiy <bytefu@gmail.com>
2011-11-23 02:00:30 +0300

    Fixed the server address to allow connections not only from localhost.

--- gdbserver/gdb-server.c
+++ gdbserver/gdb-server.c
@@ -732,7 +732,7 @@ int serve(stlink_t *sl, int port) {
 #else // not Windows
 	struct sockaddr_in serv_addr = {0};
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_addr.sin_addr.s_addr = 0;
 	serv_addr.sin_port = htons(port);
 
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
