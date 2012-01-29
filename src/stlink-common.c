0f44587d734f2b41391cd1afa077f6d5f6fa8161
Artem Borisovskiy <bytefu@gmail.com>
2012-01-30 01:40:38 +0300

    Cast fprintf() arguments to unsigned according to format specifiers.

--- src/stlink-common.c
+++ src/stlink-common.c
@@ -1484,7 +1484,7 @@ int stlink_write_flash(stlink_t *sl, stm32_addr_t addr, uint8_t* base, unsigned 
 
 		if ((off % sl->flash_pgsz) > (sl->flash_pgsz -5)) {
 		    fprintf(stdout, "\r%3u/%u pages written", 
-			    off/sl->flash_pgsz, len/sl->flash_pgsz);
+			    (unsigned)(off/sl->flash_pgsz), (unsigned)(len/sl->flash_pgsz));
 		    fflush(stdout);
 		}
 
